808584d81283c208de8e27241d4fbc2ba6710cb1
meet <meetshah3312@gmail.com>
2023-06-25 16:21:52 +0530

    first commit

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+n1=int(input("Please Enter marks 1:"))
+n2=int(input("Please Enter Marks 2:"))
+n3=int(input("Please Enter Marks 3:"))
+num=n1+n2+n3
+avg=num/3
+print("The Average of n1,n2,n3 is",avg)
